0e3c0eac449282d908c4ac6557423a3f9d04d36e
dermatologist <github@gulfdoctor.net>
2019-09-21 20:43:12 -0400

    feat: Add image upload html

--- template/fastai-vision/function/handler.py
+++ template/fastai-vision/function/handler.py
@@ -1,6 +1,6 @@
 import json
 import os
-#from pathlib import PurePath
+# from pathlib import PurePath
 import base64
 from fastai import *
 from fastai.vision import *
@@ -8,20 +8,23 @@ from io import BytesIO
 
 # No warnings in production
 import warnings
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 FUNCTION_ROOT = os.environ.get("function_root", "/root/function")
 MODEL_FILE = os.environ.get("model_file", "export.pkl")
 
 learn = load_learner(FUNCTION_ROOT + '/model/', MODEL_FILE)
 
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
+
+    if req is empty try will fail and the except will display image upload page.
+    Upload page submits to the function.
     """
-    result = {}
     try:
 
         event = json.loads(req)
@@ -33,14 +36,13 @@ def handle(req):
             "function": "Function",
             "prediction": str(prediction)
         }
+        return json.dumps(result)
 
     except:
-        result = {
-            "function": "Error",
-            "prediction": "Error"
-        }
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, 'html', 'upload.html')
 
-    return json.dumps(result)
+        with(open(path, 'r')) as file:
+            html = file.read()
 
-
-
+        return html
