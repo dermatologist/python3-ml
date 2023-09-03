322c36c0acb685ceb968c8f5e10cd50da5816a3a
Bell Eapen <github@gulfdoctor.net>
2023-09-02 22:00:26 -0400

    update 5 files and delete 8 files

--- template/conda-langroid/index.py
+++ template/conda-langroid/index.py
@@ -1,41 +1,73 @@
-# Copyright (c) Alex Ellis 2017. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+"""
+The most basic chatbot example, using the default settings.
+A single Agent allows you to chat with a pre-trained Language Model.
+"""
+import typer
+from rich import print
+from rich.prompt import Prompt
 
-from flask import Flask, request
-from function import handler
-from waitress import serve
-import os
+from langroid.agent.chat_agent import ChatAgent, ChatAgentConfig
+from langroid.agent.task import Task
+from langroid.language_models.openai_gpt import OpenAIChatModel, OpenAIGPTConfig
+from langroid.utils.configuration import set_global, Settings
+from langroid.utils.logging import setup_colored_logging
 
-app = Flask(__name__)
 
-# distutils.util.strtobool() can throw an exception
-def is_true(val):
-    return len(val) > 0 and val.lower() == "true" or val == "1"
+app = typer.Typer()
 
-@app.before_request
-def fix_transfer_encoding():
-    """
-    Sets the "wsgi.input_terminated" environment flag, thus enabling
-    Werkzeug to pass chunked requests as streams.  The gunicorn server
-    should set this, but it's not yet been implemented.
-    """
+setup_colored_logging()
 
-    transfer_encoding = request.headers.get("Transfer-Encoding", None)
-    if transfer_encoding == u"chunked":
-        request.environ["wsgi.input_terminated"] = True
 
-@app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
-@app.route("/<path:path>", methods=["POST", "GET"])
-def main_route(path):
-    raw_body = os.getenv("RAW_BODY", "false")
+def chat() -> None:
+    print(
+        """
+        [blue]Welcome to the basic chatbot!
+        Enter x or q to quit at any point.
+        """
+    )
+    sys_msg = Prompt.ask(
+        "[blue]Tell me who I am. Hit Enter for default, or type your own\n",
+        default="Default: 'You are a helpful assistant'",
+    )
 
-    as_text = True
+    config = ChatAgentConfig(
+        system_message=sys_msg,
+        llm=OpenAIGPTConfig(
+            chat_model=OpenAIChatModel.GPT4,
+        ),
+    )
+    agent = ChatAgent(config)
+    task = Task(
+        agent,
+        system_message="""
+        You are a helpful assistant. Be very concise in your responses, use no more
+        than 1-2 sentences.
+        """,
+    )
+    task.run()
 
-    if is_true(raw_body):
-        as_text = False
 
-    ret = handler.handle(request.get_data(as_text=as_text))
-    return ret
+@app.command()
+def main(
+    debug: bool = typer.Option(False, "--debug", "-d", help="debug mode"),
+    no_stream: bool = typer.Option(
+        False, "--nostream", "-ns", help="no streaming"),
+    nocache: bool = typer.Option(
+        False, "--nocache", "-nc", help="don't use cache"),
+    cache_type: str = typer.Option(
+        "redis", "--cachetype", "-ct", help="redis or momento"
+    ),
+) -> None:
+    set_global(
+        Settings(
+            debug=debug,
+            cache=not nocache,
+            stream=not no_stream,
+            cache_type=cache_type,
+        )
+    )
+    chat()
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app()
